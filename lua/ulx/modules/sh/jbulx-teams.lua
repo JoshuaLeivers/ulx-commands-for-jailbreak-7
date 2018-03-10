83f0aea151a6212c18ecdc1451466ed4d413427c
VulpusMaximus <vulpusdev@gmail.com>
2018-03-10 13:15:42 +0000

    Update jbulx-teams.lua

--- lua/ulx/modules/sh/jbulx-teams.lua
+++ lua/ulx/modules/sh/jbulx-teams.lua
@@ -1,42 +1,54 @@
 local error_not_jailbreak = "The current gamemode is not jailbreak!"
 
 
-function ulx.makeguard( calling_ply, target_ply )
+function ulx.makeguard( calling_ply, target_plys )
 	if GAMEMODE_NAME == "jailbreak" then
-		target_ply:SetTeam(TEAM_GUARD);
-		target_ply:KillSilent();
-		target_ply:SendNotification("Forced to guards");
-		ulx.fancyLogAdmin( calling_ply, "#A made #T a guard",  target_ply );
+		local affected_plys = {}
+		for k,v in pairs(target_plys) do
+			v:SetTeam(TEAM_GUARD);
+			v:KillSilent();
+			v:SendNotification("Forced to guards");
+			table.insert(affected_plys,v)
+		end
+		ulx.fancyLogAdmin(calling_ply,"#A forced #T to guards",affected_plys)
 	else
 		ULib.tsayError(calling_ply, error_not_jailbreak, true);
 	end
 end
 local makeguard = ulx.command("Jailbreak", "ulx makeguard", ulx.makeguard, "!makeguard")
 makeguard:defaultAccess( ULib.ACCESS_ADMIN )
-makeguard:addParam{ type=ULib.cmds.PlayerArg }
-makeguard:help( "Makes target a guard." )
+makeguard:addParam{ type=ULib.cmds.PlayersArg }
+makeguard:help( "Forces target(s) to guard team." )
 
-function ulx.makeprisoner( calling_ply, target_ply )
+function ulx.makeprisoner( calling_ply, target_plys )
 	if GAMEMODE_NAME == "jailbreak" then
-		target_ply:SetTeam(TEAM_PRISONER);
-		target_ply:KillSilent();
-		target_ply:SendNotification("Forced to prisoners");
-		ulx.fancyLogAdmin( calling_ply, "#A made #T a prisoner",  target_ply );
+		local affected_plys = {}
+		for k,v in pairs(target_plys) do
+			v:SetTeam(TEAM_PRISONER);
+			v:KillSilent();
+			v:SendNotification("Forced to prisoners");
+			table.insert(affected_plys,v)
+		end
+		ulx.fancyLogAdmin(calling_ply,"#A forced #T to prisoners",affected_plys)
 	else
 		ULib.tsayError(calling_ply, error_not_jailbreak, true);
 	end
 end
 local makeprisoner = ulx.command("Jailbreak", "ulx makeprisoner", ulx.makeprisoner, "!makeprisoner" )
 makeprisoner:defaultAccess( ULib.ACCESS_ADMIN )
-makeprisoner:addParam{ type=ULib.cmds.PlayerArg }
-makeprisoner:help( "Makes target a prisoner." )
+makeprisoner:addParam{ type=ULib.cmds.PlayersArg }
+makeprisoner:help( "Forces target(s) to prisoner team." )
 
-function ulx.makespectator( calling_ply, target_ply )
+function ulx.makespectator( calling_ply, target_plys )
 	if GAMEMODE_NAME == "jailbreak" then
-		target_ply:SetTeam(TEAM_SPECTATOR);
-		target_ply:KillSilent();
-		target_ply:SendNotification("Forced to spectators");
-		ulx.fancyLogAdmin( calling_ply, "#A made #T a spectator",  target_ply );
+		local affected_plys = {}
+		for k,v in pairs(target_plys) do
+			v:SetTeam(TEAM_SPECTATOR);
+			v:KillSilent();
+			v:SendNotification("Forced to spectators");
+			table.insert(affected_plys,v)
+		end
+		ulx.fancyLogAdmin(calling_ply,"#A forced #T to spectators",affected_plys)
 	else
 		ULib.tsayError(calling_ply, error_not_jailbreak, true);
 	end
