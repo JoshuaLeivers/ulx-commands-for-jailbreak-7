6488152bbd0ad2c77986141dc4bb164d46b3d711
Vulpus <vulpus@vulpus.dev>
2022-06-27 20:49:31 +0100

    Add forceprisoner+spectator and add gamemode check
    - Add forceprisoner and forcespectator
    - Add condition to only add teams module commands in Jail Break

--- lua/ulx/modules/sh/jb7-teams.lua
+++ lua/ulx/modules/sh/jb7-teams.lua
@@ -6,20 +6,28 @@
 ]]
 
 
-if GAMEMODE_NAME == "jailbreak" then -- Only enable these commands when loaded in Jail Break gamemode
-
 local CATEGORY_NAME = "Jail Break"
+local ERROR_GAMEMODE = "That command only works in Jail Break!"
 
 local CONCOMMAND_GUARD = "jb_team_select_guard"
 local CONCOMMAND_PRISONER = "jb_team_select_prisoner"
 local CONCOMMAND_SPECTATOR = "jb_team_select_spectator"
 
+
+-- ULX Commands
+
 function ulx.forceguard( calling_ply, target_plys )
+    -- If not currently loaded into Jail Break, fail and tell the player
+    if GAMEMODE_NAME ~= "jailbreak" then
+        ULib.tsayError( calling_ply, ERROR_GAMEMODE, true )
+        return
+    end
+
     -- Store list of successfully affected targets
     local affected_plys = {}
     local unaffected_plys = {}
 
-    -- Go through targets, changing their team if successful
+    -- Go through targets, adding them to the un/affected players lists as necessary
     for _, ply in ipairs( target_plys ) do
         if ply:Team() == TEAM_GUARD then -- Player is already a guard
             table.insert( unaffected_plys, ply )
@@ -75,31 +83,124 @@ function ulx.forceguard( calling_ply, target_plys )
     -- Log action
     ulx.fancyLogAdmin( calling_ply, "#A forced #T to guards", affected_plys )
 end
-local forceguard = ulx.command( CATEGORY_NAME, "ulx forceguard", ulx.forceguard, { "!forceguard", "!fguard" }, true )
-forceguard:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
-forceguard:defaultAccess( ULib.ACCESS_ADMIN )
-forceguard:help( "Forces target(s) to guard team." )
+local forceguard
 
 
---[[
 function ulx.forceprisoner( calling_ply, target_plys )
-    
-end
-local forceprisoner = ulx.command( CATEGORY_NAME, "ulx forceprisoner", ulx.forceprisoner, { "!forceprisoner", "!fprisoner" }, true )
-forceprisoner:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
-forceprisoner:defaultAccess( ULib.ACCESS_ADMIN )
-forceprisoner:help( "Forces target(s) to prisoner team." )
-]]
+    -- If not currently loaded into Jail Break, fail and tell the player
+    if GAMEMODE_NAME ~= "jailbreak" then
+        ULib.tsayError( calling_ply, ERROR_GAMEMODE, true )
+        return
+    end
 
---[[
-function ulx.forcespectator( calling_ply, target_plys )
-    
+    -- Store list of successfully affected targets
+    local affected_plys = {}
+    local unaffected_plys = {}
+
+    -- Go through targets, adding them to the un/affected players lists as necessary
+    for _, ply in ipairs( target_plys ) do
+        if ply:Team() == TEAM_PRISONER then -- Player is already a prisoner
+            table.insert( unaffected_plys, ply )
+        else
+            table.insert( affected_plys, ply )
+        end
+    end
+
+    -- If any targets couldn't be switched, tell the player
+    if #unaffected_plys > 0 then
+        -- Generate a comma-separated list of unaffected targets
+        local msg = ""
+        for k, ply in ipairs( unaffected_plys ) do
+            if k ~= 1 then
+                msg = msg .. "," .. ply:Nick()
+            else
+                msg = msg .. ply:Nick()
+            end
+        end
+
+        -- Format message based on if plural or not
+        if #unaffected_plys > 1 then
+            msg = msg .. " are already prisoners!"
+        else
+            msg = msg .. " is already a prisoner!"
+        end
+
+        -- Send message
+        ULib.tsayError( calling_ply, msg, true )
+    end
+
+    -- If no targets were able to be affected, just stop here
+    if #affected_plys == 0 then return end
+
+    -- Switch the affected targets' teams
+    for _, ply in ipairs( affected_plys ) do
+        ply:SendNotification( "Admin switched you to prisoners" )
+        ply:ConCommand( CONCOMMAND_PRISONER )
+    end
+
+    -- Log action
+    ulx.fancyLogAdmin( calling_ply, "#A forced #T to prisoners", affected_plys )
 end
-local forcespectator = ulx.command( CATEGORY_NAME, "ulx forcespectator", ulx.forcespectator, { "!forcespectator", "!forcespec", "!fspectator", "!fspec", }, true )
-forcespectator:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
-forcespectator:defaultAccess( ULib.ACCESS_ADMIN )
-forcespectator:help( "Forces target(s) to spectator team." )
-]]
+local forceprisoner
+
+
+function ulx.forcespectator( calling_ply, target_plys )
+    -- If not currently loaded into Jail Break, fail and tell the player
+    if GAMEMODE_NAME ~= "jailbreak" then
+        ULib.tsayError( calling_ply, ERROR_GAMEMODE, true )
+        return
+    end
+
+    -- Store list of successfully affected targets
+    local affected_plys = {}
+    local unaffected_plys = {}
+
+    -- Go through targets, adding them to the un/affected players lists as necessary
+    for _, ply in ipairs( target_plys ) do
+        if ply:Team() == TEAM_SPECTATOR then -- Player is already a spectator
+            table.insert( unaffected_plys, ply )
+        else
+            table.insert( affected_plys, ply )
+        end
+    end
+
+    -- If any targets couldn't be switched, tell the player
+    if #unaffected_plys > 0 then
+        -- Generate a comma-separated list of unaffected targets
+        local msg = ""
+        for k, ply in ipairs( unaffected_plys ) do
+            if k ~= 1 then
+                msg = msg .. "," .. ply:Nick()
+            else
+                msg = msg .. ply:Nick()
+            end
+        end
+
+        -- Format message based on if plural or not
+        if #unaffected_plys > 1 then
+            msg = msg .. " are already spectators!"
+        else
+            msg = msg .. " is already a spectator!"
+        end
+
+        -- Send message
+        ULib.tsayError( calling_ply, msg, true )
+    end
+
+    -- If no targets were able to be affected, just stop here
+    if #affected_plys == 0 then return end
+
+    -- Switch the affected targets' teams
+    for _, ply in ipairs( affected_plys ) do
+        ply:SendNotification( "Admin switched you to spectators" )
+        ply:ConCommand( CONCOMMAND_SPECTATOR )
+    end
+
+    -- Log action
+    ulx.fancyLogAdmin( calling_ply, "#A forced #T to spectators", affected_plys )
+end
+local forcespectator
+
 
 --[[
 function ulx.forcewarden( calling_ply, target_ply, override )
@@ -133,4 +234,26 @@ rebel:help( "Declare target(s) as rebel(s).")
 rebel:setOpposite( "ulx pardon", { _, _, true }, "!pardon", true )
 ]]
 
-end -- End of gamemode if-statement
+-- Register commands on GM:Initialize
+-- GAMEMODE_NAME isn't initialized at the time ULX modules are loaded
+hook.Add( "Initialize", "jb7-ulx_teams_initialize", function()
+    if GAMEMODE_NAME == "jailbreak" then
+        -- Load forceguard
+        forceguard = ulx.command( CATEGORY_NAME, "ulx forceguard", ulx.forceguard, { "!forceguard", "!fguard" }, true )
+        forceguard:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
+        forceguard:defaultAccess( ULib.ACCESS_ADMIN )
+        forceguard:help( "Forces target(s) to guard team." )
+
+        -- Load forceprisoners
+        forceprisoner = ulx.command( CATEGORY_NAME, "ulx forceprisoner", ulx.forceprisoner, { "!forceprisoner", "!fprisoner" }, true )
+        forceprisoner:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
+        forceprisoner:defaultAccess( ULib.ACCESS_ADMIN )
+        forceprisoner:help( "Forces target(s) to prisoner team." )
+
+        -- Load forcespectator command
+        forcespectator = ulx.command( CATEGORY_NAME, "ulx forcespectator", ulx.forcespectator, { "!forcespectator", "!forcespec", "!fspectator", "!fspec", }, true )
+        forcespectator:addParam{ type=ULib.cmds.PlayersArg, default="^", ULib.cmds.optional }
+        forcespectator:defaultAccess( ULib.ACCESS_ADMIN )
+        forcespectator:help( "Forces target(s) to spectator team." )
+    end
+end )
